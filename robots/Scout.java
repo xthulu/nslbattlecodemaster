396c89ed9e6c01f45b6c023616f4ee4bb4d2df7d
alando46 <music2177@gmail.com>
2017-01-15 13:11:16 -0800

    work in progress

--- robots/Scout.java
+++ robots/Scout.java
@@ -15,6 +15,9 @@ public strictfp class Scout extends NSLRobot {
     }
 
     @SuppressWarnings("unused")
+    // new - alando
+    RobotType selfData;
+    // lots of new - alando
     public void run() throws GameActionException {
 
         MapLocation myLocation = rc.getLocation();
@@ -22,22 +25,19 @@ public strictfp class Scout extends NSLRobot {
         // See if there are any nearby enemy robots
         RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
         // Check to see what type of enemy robot it is
-
         for (int i = 0; i <= robots.length; i++)
         {
             if (robots[i].getType() == RobotType.ARCHON) {
-                int targetID = robots[i].getID();
+
                 // is the below the best way to pass parameters?
-                Util.lockOn(targetID, "ARCHON", myLocation);
+                Util.lockOn(robots[i], myLocation);
                 // TODO lock-on (don't let escape)
                 // engage
                 // broadcast location
 
             }
-
-
-
-                    // If there are some...
+        // end of alando work
+        // If there are some...
 
         } if (robots.length > 0) {
             // And we have enough bullets, and haven't attacked yet this turn...
--- util/Util.java
+++ util/Util.java
@@ -1,20 +1,28 @@
 package nslbattlecodemaster.util;
-
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import nslbattlecodemaster.robots.Scout.*
 import battlecode.common.*;
 
 /**
  * Created by morris on 1/11/17.
+ * adding lockOn on 1/15/17 - ballantyne.
  */
 public class Util {
-
-    public static void lockOn(int targetId, String type, MapLocation myLocation) {
+        // all new alando stuff
+    public static void lockOn(RobotInfo targetInfo, RobotInfo attackerInfo, MapLocation myLocation) {
         // move so that (targetPosition - selfPosition) = (maxShootingRange - (maxTargetMovement/turn))
 
-        int target = targetId;
-        int strideRadius = RobotType[type].valueOf(strideRadius);
-        int distanceToTarget = targetLocation - myLocation
+        int targetID = targetInfo.ID;
+        MapLocation targetLocation = targetInfo.location;
+
+        float targStrideRadius = targetInfo.type.strideRadius;
+        float distanceToTarget = targetLocation.distanceTo(myLocation);
+        float bulletRange =
+
         while (true) {
-            if
+            if (distanceToTarget < ) {
+
+            }
 
 
 
