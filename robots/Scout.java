047a9cfcda34b5445edc2930331eaa10a43a8c45
alando46 <music2177@gmail.com>
2017-01-14 22:05:13 -0800

    building lockOn()

--- robots/Scout.java
+++ robots/Scout.java
@@ -1,9 +1,11 @@
 package nslbattlecodemaster.robots;
 
 import battlecode.common.*;
+import nslbattlecodemaster.util.Util;
 
 /**
  * Created by morris on 1/11/17.
+ * Added enemy prioritization - ballantyne 1/14/17
  */
 public strictfp class Scout extends NSLRobot {
 
@@ -19,9 +21,25 @@ public strictfp class Scout extends NSLRobot {
 
         // See if there are any nearby enemy robots
         RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
+        // Check to see what type of enemy robot it is
 
-        // If there are some...
-        if (robots.length > 0) {
+        for (int i = 0; i <= robots.length; i++)
+        {
+            if (robots[i].getType() == RobotType.ARCHON) {
+                int targetID = robots[i].getID();
+                // is the below the best way to pass parameters?
+                Util.lockOn(targetID, "ARCHON", myLocation);
+                // TODO lock-on (don't let escape)
+                // engage
+                // broadcast location
+
+            }
+
+
+
+                    // If there are some...
+
+        } if (robots.length > 0) {
             // And we have enough bullets, and haven't attacked yet this turn...
             if (rc.canFireSingleShot()) {
                 // ...Then fire a bullet in the direction of the enemy.
--- util/Util.java
+++ util/Util.java
@@ -7,6 +7,20 @@ import battlecode.common.*;
  */
 public class Util {
 
+    public static void lockOn(int targetId, String type, MapLocation myLocation) {
+        // move so that (targetPosition - selfPosition) = (maxShootingRange - (maxTargetMovement/turn))
+
+        int target = targetId;
+        int strideRadius = RobotType[type].valueOf(strideRadius);
+        int distanceToTarget = targetLocation - myLocation
+        while (true) {
+            if
+
+
+
+        }
+    }
+
 
 
 }
